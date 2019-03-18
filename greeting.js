38926f1364ccc6e30844528708e75a348c53237d
박선영 <sunsmile01023@gmail.com>
2019-03-19 03:51:43 +0900

    clock.js 로 현재 시각 띄우기 완료

---
+++ greeting.js
@@ -0,0 +1,7 @@
+function init(){
+
+}
+
+init();
+
+//local storage: javascript의 작은 정보를 저장
---
+++ index.js
@@ -0,0 +1,23 @@
+const title=document.querySelector("#title");
+
+const CLICKED_CLASS="clicked";
+
+function handleClick(){
+  /*
+  const hasClass=title.classList.contains(CLICKED_CLASS);
+
+  if(!hasClass){
+    title.classList.add(CLICKED_CLASS);
+  }else{
+    title.classList.remove(CLICKED_CLASS);
+  }
+  */
+
+  //클래스가 없으면 add, 있으면 remove 
+  title.classList.toggle(CLICKED_CLASS);
+}
+function init(){
+  title.addEventListener("click",handleClick);
+}
+
+init();
