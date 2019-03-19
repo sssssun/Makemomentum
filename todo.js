db0d2b8160b4b5c724a1a50d13577c2ff7d696f1
박선영 <sunsmile01023@gmail.com>
2019-03-19 22:28:45 +0900

    todo.js 로 todoList input submit 하면 밑에 내용 표시

--- todo.js
+++ todo.js
@@ -1,5 +1,41 @@
-function init(){
+const toDoform=document.querySelector(".js-toDoform");
+const toDoInput=toDoform.querySelector("input");
+const toDoList=document.querySelector(".js-toDoList");
 
+const TODO_LS='todo';
+const TOCO_CN='showingtodo';
+
+
+
+function showTodo(text){
+  const li=document.createElement("li");
+  const deleteBtn=document.createElement("delete");
+  const span=document.createElement("span");
+  span.innerText=text;
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue=toDoInput.value;
+  showTodo(currentValue);
+  toDoInput.value="";
+}
+
+function loadTodo(){
+  const todo=localStorage.getItem(TODO_LS);
+
+  if(todo!==null){
+    showTodo();
+  }
+}
+
+
+function init(){
+  loadTodo();
+  toDoform.addEventListener("submit",handleSubmit);
 }
 
 init();
